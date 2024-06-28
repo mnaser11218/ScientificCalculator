b385c00b1793ee1a68b3ca80623182e27aa01059
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-06-28 17:21:04 -0400

    added a space

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -11,7 +11,7 @@ public class MainApplication {
         Double d = Console.getDoubleInput("Enter a double.");
 
         Console.println("The user input %s as a string ", s);
-        Console.println("The user input %s as a integer", i);
+        Console.println("The user input %s as a integer ", i);
         Console.println("The user input %s as a d", d);
     }
 }
